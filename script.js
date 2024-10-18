8e2509de8d2874d76e1a58ceaac3bc7ff62afd4d
feassh <feassh@gmail.com>
2024-10-18 23:57:52 +0800

    Update script.js

--- script.js
+++ script.js
@@ -127,7 +127,7 @@ function makeRequest(url, isPreview, text) {
         url: url,
         method: 'GET',
         headers: {
-            'x-api-key': '@ak47'
+            'x-api-key': 'feassh'
         },
         xhrFields: {
             responseType: 'blob'
